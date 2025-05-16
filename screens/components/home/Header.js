4bb0d83d9a6d3a58b6713c96c990dd715cd3f9ca
horodeski <geovana.horodeski06@gmail.com>
2025-05-15 22:49:50 -0300

    feat: format date in Portuguese

--- screens/components/home/Header.js
+++ screens/components/home/Header.js
@@ -2,10 +2,21 @@ import React from "react";
 import { Text, View } from "react-native";
 
 export default function Header() {
+	const today = new Date();
+	const formattedDate = today.toLocaleDateString("pt-BR", {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+	});
+
+	const capitalizedDate = formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+
 	return (
 		<View style={{ marginTop: 100, marginBottom: 50 }}>
-			<Text style={{ fontFamily: "BebasNeue-Regular", color: "#edf0ce",fontSize: 50}}>OVERVIEW</Text>
-			<Text style={{ color: "#edf0ce"}}>Wednesday, May 20</Text>
+			<Text style={{ fontFamily: "BebasNeue-Regular", color: "#edf0ce", fontSize: 50 }}>
+				OVERVIEW
+			</Text>
+			<Text style={{ color: "#edf0ce" }}>{capitalizedDate}</Text>
 		</View>
 	);
 }
